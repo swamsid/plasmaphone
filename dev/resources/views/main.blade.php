422c7084144b009dea81d7e665abfd3f454f4a33
foxbloodiron <ariakbar6944@gmail.com>
2018-07-23 15:57:28 +0700

    push disek ben gk salah paham

---
+++ dev/resources/views/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+@include('layouts._head')
+
+@yield('extra_styles')
+
+<body>
+	@include('layouts._sidebar')
+
+	@yield('content')
+
+</body>
+
+@include('layouts._script')
+
+@yield('extra_scripts')
+
+
+
+</html>
