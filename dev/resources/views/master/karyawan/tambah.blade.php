52d94ae7282c4b35acfefbc01f286378867ae63a
dirga <swamsid@gmail.com>
2018-08-20 10:03:45 +0700

    cek

--- dev/resources/views/master/karyawan/tambah.blade.php
+++ dev/resources/views/master/karyawan/tambah.blade.php
@@ -3,84 +3,65 @@
 @section('title', 'Tambah Master User')
 
 @section('content')
-<section class="content">
-    <div class="container-fluid">
+    <section class="content">
+        <div class="container-fluid">
 
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 head-button">
-                <div class="card" >
-                    <div class="body" style="padding: 0px 0px;">
-                        <div class="col-md-2 text-center active">
-                            <a href="{{ route('karyawan.index') }}">
-                                <i class="fa fa-table fa-fw"></i> &nbsp;Data Tabel
-                            </a>
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 head-button">
+                    <div class="card" >
+                        <div class="body" style="padding: 0px 0px;">
+                            <div class="col-md-2 text-center active">
+                                <a href="{{ route('karyawan.index') }}">
+                                    <i class="fa fa-table fa-fw"></i> &nbsp;Data Tabel
+                                </a>
+                            </div>
+
+                            <div class="col-md-2 text-center">
+                                <a href="{{ route('karyawan.create') }}">
+                                    <i class="fa fa-plus fa-fw"></i> &nbsp;Tambahkan Data
+                                </a>
+                            </div>
+
+                            <div class="col-md-2 text-center">
+                                <i class="fa fa-pencil-square-o fa-fw"></i> &nbsp;Edit Data
+                            </div>
+
+                            <div class="col-md-2 text-center">
+                                <i class="fa fa-eraser fa-fw"></i> &nbsp;Hapus Data
+                            </div>
                         </div>
-
-                        <div class="col-md-2 text-center">
-                            <a href="{{ route('karyawan.create') }}">
-                                <i class="fa fa-plus fa-fw"></i> &nbsp;Tambahkan Data
-                            </a>
+                    </div>
+                </div>
+            </div>       
+            
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                Tambah Data Master User
+                            </h2>
                         </div>
-
-                        <div class="col-md-2 text-center">
-                            <i class="fa fa-pencil-square-o fa-fw"></i> &nbsp;Edit Data
-                        </div>
-
-                        <div class="col-md-2 text-center">
-                            <i class="fa fa-eraser fa-fw"></i> &nbsp;Hapus Data
+                        <div class="body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>       
-        
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="header">
-                        <h2>
-                            Tambah Data Master User
-                        </h2>
-                    </div>
-                    <div class="body">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-                                    <label>Input 1</label>
-                                </div>
-                                <div class="col-md-8 col-sm-8 col-xs-12">
-                                    <div class="form-group form-group-sm">
-                                        <div class="form-line">
-                                            <input type="text" class="form-control" name="" placeholder="Username">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-6">
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-                                    <label>Input 2</label>
-                                </div>
-                                <div class="col-md-8 col-sm-8 col-xs-12">
-                                    <div class="form-group form-group-sm">
-                                        <div class="form-line">
-                                            <input type="text" class="form-control" name="" placeholder="Username">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
-        
-    </div>
-</section>
+    </section>
 @endsection
+
 @section('extra_scripts')
-<script type="text/javascript">
-    
-</script>
+
+    <script type="text/javascript">
+        
+    </script>
+
 @endsection
+
